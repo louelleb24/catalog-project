571d4b348d00eee2899e025c48185cf0d508f678
louelleb24 <louelleb24@gmail.com>
2024-04-08 18:32:27 -0700

    initialcommit

--- scripts.js
+++ scripts.js
@@ -23,17 +23,31 @@
  * 
  */
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+// Array of URLs for the images 
+let pictureURLs = [
+    "https://therecipecritic.com/wp-content/uploads/2023/02/soup_dumplings.jpg",
+    "https://pbs.twimg.com/media/EoZzAZzWEAIfMGc.jpg:large",
+    "https://thecommunalfeast.com/wp-content/uploads/2021/09/2A94FB4E-8797-4BBA-9192-478DB91729F0-scaled.jpeg"
 ];
+
+// Array for bullet points underneath pictures for more information 
+let bulletPoints = [
+    ["Amazing soup filled dumplings with great flavor", "Perfect for cold days", "Delicious taste and flavor"],
+    ["Fluffy waffles infused with ube", "Topped with ube butter", "Great way to spice up waffles"], 
+    ["Amazing authentic street tacos", "Topped with mango salsa", "Great tacos to try"]
+
+];
+
+// This is an array of strings (Food titles)
+let titles = [
+    "Soup Dumplings",
+    "Ube Waffles",
+    "Tacos Al Pastor"
+];
+
+
+
+
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
@@ -43,28 +57,37 @@ function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
 
+    const infoData = [
+    {titlePicture: titles[0], pictureURL: pictureURLs[0]},
+    {titlePicture: titles[1], pictureURL: pictureURLs[1]},
+    {titlePicture: titles[2], pictureURL: pictureURLs[2]},
+
+    ]; 
+
+    const bulletData = [
+        {bulletPoint: bulletPoints[0]},
+        {bulletPoint: bulletPoints[1]},
+        {bulletPoint: bulletPoints[2]},
+
+    ];
+
+
+    
+    for (let i = 0; i < titles.length; i++) { 
         // This part of the code doesn't scale very well! After you add your
         // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
+        const {titlePicture, pictureURL} = infoData[i]; 
+        const bulletPointsForCard = bulletData[i];
 
         const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
+        editCardContent(nextCard, titlePicture, pictureURL, bulletPointsForCard); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
     }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+
+function editCardContent(card, newTitle, newImageURL, bulletPoints) {
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
@@ -74,11 +97,24 @@ function editCardContent(card, newTitle, newImageURL) {
     cardImage.src = newImageURL;
     cardImage.alt = newTitle + " Poster";
 
+    // Get the ul element where bullet points will be added
+    const bulletList = card.querySelector(".bullet-points");
+    bulletList.innerHTML = ""; // clears existing bullet points data 
+
+    // Loop through each bullet point and create a new li element 
+    bulletPoints.forEach(bulletPoint => {
+        const listItem = document.createElement("li");
+        listItem.textContent = bulletPoint; 
+        bulletList.appendChild(listItem);
+    })
+  
+
     // You can use console.log to help you debug!
     // View the output by right clicking on your website,
     // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
-}
+
+    console.log("new card title:", newTitle, "- html content: ", card.outerHTML);
+} // cardouterHTML was missing a dot in between card and outer 
 
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
@@ -89,6 +125,10 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+    if (titles.length > 0) {
+        titles.pop(); // Remove last item in titles array 
+        showCards(); // Call showCards 
+    } else {
+        alert("There are no more cards to remove");
+    }
 }
